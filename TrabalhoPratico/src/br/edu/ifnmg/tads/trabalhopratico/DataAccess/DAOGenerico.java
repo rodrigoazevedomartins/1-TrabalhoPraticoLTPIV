6223d1da35c710b513541c4e171908a353666353
aluno <aluno@aluno-desktop>
2013-09-16 10:59:22 -0300

    Criacao do DAOGenerico e FuncionarioDAO

---
+++ TrabalhoPratico/src/br/edu/ifnmg/tads/trabalhopratico/DataAccess/DAOGenerico.java
@@ -0,0 +1,104 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifnmg.tads.trabalhopratico.DataAccess;
+
+import javax.persistence.Entity;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+/**
+ *
+ * @author Rodrigo
+ */
+public abstract class DAOGenerico<T> {
+    
+    private EntityManager manager;
+    private EntityManagerFactory factory;
+    private Class tipo;
+    
+    public DAOGenerico(Class t){
+        
+        factory = Persistence.createEntityManagerFactory("trabalhopraticoPU");
+        manager = factory.createEntityManager();
+        tipo = t;
+        
+    }
+    
+    
+    public boolean Salvar(T obj){
+        
+        EntityTransaction transaction = manager.getTransaction();
+        
+        try{
+            transaction.begin();
+            
+            manager.persist(obj);
+            
+            transaction.commit();
+            
+            return true;
+        
+        } catch(Exception ex){
+            
+            transaction.rollback();
+            return false;
+        
+        }
+        
+        
+        
+        
+    }
+    
+    public T Abrir(long id){
+        
+        EntityTransaction transaction = manager.getTransaction();
+        
+        try{
+            
+            T obj = (T)manager.find(tipo, id); 
+            
+            return obj;
+            
+        } catch(Exception ex){
+            
+            return null;
+      
+        }
+        
+    }
+    
+    public boolean Excluir(T obj){
+        
+        EntityTransaction transaction = manager.getTransaction();
+        
+        try{
+            
+            transaction.begin();
+            
+            manager.merge(obj);
+            
+            transaction.commit();
+            
+            return true;
+            
+        } catch(Exception ex){
+            
+            transaction.rollback();
+            
+            return false;
+        }
+        
+        
+    }
+    
+    
+    
+    
+    
+    
+}
---
+++ TrabalhoPratico/src/br/edu/ifnmg/tads/trabalhopratico/DataAccess/FuncionarioDAO.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifnmg.tads.trabalhopratico.DataAccess;
+
+/**
+ *
+ * @author Rodrigo
+ */
+public class FuncionarioDAO {
+    
+    
+    
+}
