f8e62c33e1a51d25d65e32cbc53a7fb5d70c5aed
Rodrigo Azevedo Martins <rodrigomartins.tads@gmail.com>
2013-09-22 20:43:21 -0300

    Criação da classe CursoDAO.

---
+++ TrabalhoPratico/src/br/edu/ifnmg/tads/trabalhopratico/DataAccess/CursoDAO.java
@@ -0,0 +1,55 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.edu.ifnmg.tads.trabalhopratico.DataAccess;
+
+import br.edu.ifnmg.tads.trabalhopratico.DomainModel.Curso;
+import java.util.HashMap;
+import java.util.List;
+import javax.persistence.Query;
+
+/**
+ *
+ * @author Rodrigo
+ */
+public class CursoDAO extends DAOGenerico<Curso>{
+    
+    public CursoDAO(){
+        super(Curso.class);
+    }
+    
+    
+    public List<Curso> buscarCurso(Curso filtro){
+        
+        String sql = "select c from Curso c, Disciplina d where c.ativo = 1";
+        
+        HashMap<String, Object> parametros = new HashMap<String, Object>();
+        
+        if(filtro.getNome().length() > 0){
+            sql = sql + " and c.nome = :c0";
+            parametros.put("c0", filtro.getNome());
+        }
+        
+        if(filtro.getDuracao() > 0){
+            sql = sql + " and c.duracao = :c1";
+            parametros.put("c1", filtro.getDuracao());
+        }
+        
+        Query consulta = manager.createQuery(sql);
+        
+        for (String par : parametros.keySet()) {
+            consulta.setParameter(par, parametros.get(par));
+        }
+       
+        return consulta.getResultList();
+        
+    }
+    
+    public List<Curso> listarTodos(){
+        
+        Query consulta = manager.createQuery("select c from Curso c, Disciplina d where c.ativo = 1");
+        return consulta.getResultList();
+        
+    }
+}
